6943494c9e4969d36a2a3d857f6346a2aaf74a2e
peng <lipengweb@gmail.com>
2016-05-28 20:52:32 +0800

    Fix some issues that cannot show the pictures.

--- app/js/services/GravatarUrlBuilder.js
+++ app/js/services/GravatarUrlBuilder.js
@@ -1,6 +1,6 @@
 'use strict';
 
-eventApp.factory('gravatarUrlBuilder', function(){
+eventsApp.factory('gravatarUrlBuilder', function(){
     return {
         buildGravatarUrl:function(email) {
              var defaultGravatarUrl = "http://www.gravatar.com/avatar/000?s=200";
